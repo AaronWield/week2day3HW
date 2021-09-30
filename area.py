4909fdfbb551582ea92b8cab1b9a11c9330c9622
Aaron Wieldraayer <Wieldraayer@Aarons-MacBook-Pro.local>
2021-09-30 09:58:40 -0400

    hello

---
+++ area.py
@@ -0,0 +1,5 @@
+def area():
+    length = int(input("What is the length of your house in feet? "))
+    width = int(input("What is the width of you house in feet? "))
+    return int(length * width)
+print(area())
---
+++ circumference.py
@@ -0,0 +1,5 @@
+from math import pi
+def circumference():
+    radius = int(input("Enter radius: "))
+    return int(radius*2*pi)
+circumference()
